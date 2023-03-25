a5f5f2421d2704e87a08bcbf504402dfe4af4693
ramakrishna-bolisetty <124112871+ramakrishna-bolisetty@users.noreply.github.com>
2023-03-25 22:14:57 +0530

    Update queryParams.js

--- queryParams.js
+++ queryParams.js
@@ -5,7 +5,6 @@ const server = http.createServer((req, res) =>{
     const queryObject = url.parse(req.url, true).query;
     const name = queryObject.name;
     if(name) {
-        // res.end(queryObject);
         res.end(`Hello, ${name}!`);
     }else{
         res.end('Please Enter the name in the URL.');
